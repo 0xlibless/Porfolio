3f0bbf11343c8831233debcdd1080d4445a9aa32
Agus <agustin.barrios.berra@gmail.com>
2026-02-06 04:09:25 -0300

    v3

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,17 @@
 import '../assets/Home.css';
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGlobe } from "react-icons/fa";
 
-export default function ProjectCard({ title, description, tags, url }) {
+export default function ProjectCard({ title, description, tags, url, icon }) {
     return (
         <a href={url} target="_blank" rel="noopener noreferrer" className="project-card glass">
             <div className="project-content">
                 <h3 className="project-title">{title}</h3>
-                <FaGithub size={30} style={{position: 'absolute', top: '60px', right: '30px'}} color="#fff" />
+                {icon == "github" && (
+                    <FaGithub size={30} style={{position: 'absolute', top: '60px', right: '30px'}} color="#fff" />
+                )}
+                {icon == "globe" && (
+                    <FaGlobe size={30} style={{position: 'absolute', top: '60px', right: '30px'}} color="#fff" />
+                )}
                 <p className="project-description">{description}</p>
                 <div className="project-tags">
                     {tags.map((tag, index) => (
