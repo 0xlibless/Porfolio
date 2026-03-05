f149051b791208a45740f401e4d02c0bd33ab0ef
Agus <101914474+0xagvz@users.noreply.github.com>
2026-03-04 21:57:14 -0300

    Adding contact section

---
+++ src/components/contact.jsx
@@ -0,0 +1,15 @@
+import '../assets/Home.css';
+import { FaGithub, FaTwitter} from "react-icons/fa";
+
+export function Contact() {
+    return (
+        <div className="contact scroll-animate">
+            <a href="https://github.com/0xlibless" target="_blank" rel="noopener noreferrer">
+                <FaGithub size={70} color="#fff"/>
+            </a>
+            <a href="https://x.com/aguatiiin" target="_blank" rel="noopener noreferrer">
+                <FaTwitter size={70} color="#fff"/>
+            </a>
+        </div>
+    );
+}
